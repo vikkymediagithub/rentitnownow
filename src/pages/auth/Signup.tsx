26a92292924ac14a3ca30956433b1280d72f95b0
oke victor joseph <okevictor14@gmail>
2025-07-06 04:41:40 +0100

    siginup

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import { Search } from "lucide-react";
+import guestImage from '../../assets/stash_user-id-light.png';
+import ownerImage from '../../assets/solar_home-line-duotone.png';
+
 
 export default function RegisterPage() {
   const [role, setRole] = useState('guest');
@@ -15,15 +18,15 @@ export default function RegisterPage() {
       <header className="bg-white border-b">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
-            <div className="flex items-center space-x-2">
-              <div className="w-8 h-8 bg-red-500 rounded flex items-center justify-center">
-                <img src="/public/favicon.png" alt="logo" />
-              </div>
-              <span className="text-xl font-bold text-gray-900">Rentitnownow.com</span>
+             <div className="flex items-center space-x-2">
+            <div className="bg-gradient-to-r from-[#F85259] to-[#3352A5] text-white rounded-md px-3 py-1 font-bold">
+              R
             </div>
-            <div className="w-8 h-8 bg-gradient-to-r from-red-500 to-blue-600 text-white rounded flex items-center justify-center">
-              <Search className="h-4 w-4 text-white" />
-            </div>
+            <p className="font-semibold text-sm">Rentitnownow.com</p>
+          </div>
+          <div className="w-8 h-8 bg-gradient-to-r from-red-500 to-blue-600 text-white rounded flex items-center justify-center">
+            <Search className="h-4 w-4 text-white" />
+          </div>
           </div>
         </div>
       </header>
@@ -42,7 +45,7 @@ export default function RegisterPage() {
                 className={`text-center py-6 px-8 bg-white rounded-lg cursor-pointer hover:shadow ${role === 'guest' ? 'shadow border border-blue-500' : ''}`}
                 onClick={() => handleRoleSwitch('guest')}
               >
-                <img src="/public/stash_user-id-light.png" alt="Guest" className="mx-auto" />
+                <img src={guestImage} alt="Guest" className="mx-auto" />
                 <h3 className="font-semibold">I am a Guest</h3>
                 <p className="font-light">Looking for a place to stay</p>
                 {role === 'guest' && (
@@ -56,7 +59,7 @@ export default function RegisterPage() {
                 className={`text-center py-6 px-8 bg-white rounded-lg cursor-pointer hover:shadow ${role === 'owner' ? 'shadow border border-blue-500' : ''}`}
                 onClick={() => handleRoleSwitch('owner')}
               >
-                <img src="/public/solar_home-line-duotone.png" alt="Owner" className="mx-auto" />
+                <img src={ownerImage} alt="Owner" className="mx-auto" />
                 <h3 className="font-semibold">I'm a Property Owner</h3>
                 <p className="font-light">Looking to list my property</p>
                 {role === 'owner' && (
